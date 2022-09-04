540e184981fec5e69f6fcecc24a0dab1ba463756
Revathi Palla <104897561+Revathi181229@users.noreply.github.com>
2022-09-04 07:55:46 +0530

    import java.util.Scanner; class PerfectSquareOrNot {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         double d;         n=sc.nextInt();         d=Math.sqrt(n);         if((d-(int)d>0))         {             System.out.println("False");         }         else         {             System.out.println("True");         }     } }

---
+++ perfect_square_root_or_not.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PerfectSquareOrNot
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        double d;
+        n=sc.nextInt();
+        d=Math.sqrt(n);
+        if((d-(int)d>0))
+        {
+            System.out.println("False");
+        }
+        else
+        {
+            System.out.println("True");
+        }
+    }
+}
