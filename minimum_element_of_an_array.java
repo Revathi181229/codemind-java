4c3d50e3f20275c0e728fe7b05947ceeae0eea36
Revathi Palla <104897561+Revathi181229@users.noreply.github.com>
2022-09-22 14:45:24 +0530

    import java.util.Scanner; class maxarr {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,max;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)             x[i]=sc.nextInt();             max=x[0];         for(i=1;i<n;i++)         {
                if(x[i]<max)
                    max=x[i];
                
            }
            System.out.println(max);
           
        }
    }

--- minimum_element_of_an_array.java
+++ minimum_element_of_an_array.java
@@ -4,20 +4,20 @@ class maxarr
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int n,x[],i,min;
+        int n,x[],i,max;
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
             x[i]=sc.nextInt();
-            min=x[0];
+            max=x[0];
         for(i=1;i<n;i++)
         {
         
-            if(x[i]<min)
-                min=x[i];
+            if(x[i]<max)
+                max=x[i];
             
         }
-        System.out.println(min);
+        System.out.println(max);
        
     }
 }
