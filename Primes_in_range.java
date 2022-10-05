99617795e74e9be54dfe2d90f541d0698e1cc61a
Revathi Palla <104897561+Revathi181229@users.noreply.github.com>
2022-10-05 17:49:09 +0530

    import  java.util.Scanner; class PR {     public static boolean Isprime(int n)     {         if(n<2)          return false;         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)              count++;         }         if(count==0)          return true;         else          return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c=0,i;         a=sc.nextInt();         b=sc.nextInt();         for(i=a;i<=b;i++)         {             if(Isprime(i))              c++;         }         System.out.println(c);
        }
    }

--- Primes_in_range.java
+++ Primes_in_range.java
@@ -1,34 +1,33 @@
-import java.util.Scanner;
-class primeinrange
+import  java.util.Scanner;
+class PR
 {
-   public static boolean isPrime(int n)
-   {
-     if(n<2)
-      return false;
-     int count=0;
-     for(int i=2;i<=(int)Math.sqrt(n);i++)
-     {
-        if(n%i==0)
-         count++;
-     }
-     if(count==0)
-      return true;
-     else
-      return false;
-     
-   }
-   public static void main(String args[ ])
-   {
-     Scanner sc=new Scanner(System.in);
-     int a,b,c=0;
-     a=sc.nextInt( );
-     b=sc.nextInt( );
-     for(int i=a;i<=b;i++)
-     {
-         if(isPrime(i))
-         c++;
-     }
-     System.out.println(c);
-     
-   }
+    public static boolean Isprime(int n)
+    {
+        if(n<2)
+         return false;
+        int count=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+             count++;
+        }
+        if(count==0)
+         return true;
+        else
+         return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c=0,i;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=a;i<=b;i++)
+        {
+            if(Isprime(i))
+             c++;
+        }
+        System.out.println(c);
+        
+    }
 }
